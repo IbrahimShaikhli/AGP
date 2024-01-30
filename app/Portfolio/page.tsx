a36bfa3c80ffc73c4eabb7ae567933db1a8123d0
Ibrahim Khalid <Ibrahim.AlShaikhli@outlook.com>
2024-01-30 23:12:04 +0400

    sizes  to update

--- app/Portfolio/page.tsx
+++ app/Portfolio/page.tsx
@@ -5,7 +5,7 @@ import NavBar from "../UI/Components/Navbar"
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-16 bg-white">
+    <main className="flex min-h-screen flex-col items-center justify-between lg:px-16 px-4 bg-white">
         <PortfolioLibrary/>
         <Footer/>
     </main>
